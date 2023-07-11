bea10661e01f30738b7b7c91a752fe2d5377e44d
Dennis Buchwald <buchwald95@icloud.com>
2023-07-11 12:49:48 +0200

    add new section YT

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,85 +4,86 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import ÜberMich from "./übermich.js";
 import Projekte from "./projekte";
+import Youtube from "./youtube";
 import Kontakt from "./kontakt";
 import Start from "./start";
 
 const Home = () => {
-  return (
-    <>
-      <a id="top" />
-      <Head>
-        <title>Dennis Buchwald</title>
-        <meta
-          name="description"
-          content="Dennis Buchwald - Kreativer Frontend-Entwickler aus Heilbronn. Entdecken Sie meine Projekte und erfahren Sie mehr über mich und meine Arbeit."
-        />
-      </Head>
-      <Header />
-      <PageContent>
-        <SectionHome>
-          <Start />
-        </SectionHome>
-        <a id="about" />
-        <Section>
-          <ÜberMich />
-        </Section>
-        <a id="projekte" />
-        <Section>
-          <Projekte />
-        </Section>
-        <Section>
-          <a id="kontakt" />
-          <Kontakt />
-        </Section>
-      </PageContent>
+	return (
+		<>
+			<a id="top" />
+			<Head>
+				<title>Dennis Buchwald</title>
+				<meta
+					name="description"
+					content="Dennis Buchwald - Kreativer Frontend-Entwickler aus Heilbronn. Entdecken Sie meine Projekte und erfahren Sie mehr über mich und meine Arbeit."
+				/>
+			</Head>
+			<Header />
+			<PageContent>
+				<SectionHome>
+					<Start />
+				</SectionHome>
+				<Section>
+					<ÜberMich />
+				</Section>
+				<Section>
+					<Projekte />
+				</Section>
+				<Section>
+					<Youtube />
+				</Section>
+				<Section>
+					<Kontakt />
+				</Section>
+			</PageContent>
 
-      <Footer />
-    </>
-  );
+			<Footer />
+		</>
+	);
 };
 
 export default Home;
 
 const PageContent = styled.div`
-  flex-grow: 1;
+	flex-grow: 1;
 `;
 
 const SectionHome = styled.section`
-  box-sizing: border-box;
-  margin: 4rem;
-  margin-top: 90px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  min-height: 100vh;
-  // background-color: ${(props) => props.theme.secondaryColor};
-  color: ${(props) => props.theme.textColor};
-  padding: 2rem;
-  border-radius: 4rem;
-  @media screen and (max-width: 768px) {
-    margin-left: 1rem;
-    margin-right: 1rem;
-    border-radius: 3rem;
-  }
+	box-sizing: border-box;
+	margin: 4rem;
+	margin-top: 90px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	min-height: 100vh;
+	// background-color: ${(props) => props.theme.secondaryColor};
+	color: ${(props) => props.theme.textColor};
+	padding: 2rem;
+	border-radius: 4rem;
+	@media screen and (max-width: 768px) {
+		margin-left: 1rem;
+		margin-right: 1rem;
+		border-radius: 3rem;
+	}
 `;
 
 const Section = styled.section`
-  box-sizing: border-box;
-  margin: 4rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  min-height: 100vh;
-  background-color: ${(props) => props.theme.secondaryColor};
-  color: ${(props) => props.theme.textColor};
-  padding: 2rem;
-  border-radius: 4rem;
-  @media screen and (max-width: 768px) {
-    margin-left: 1rem;
-    margin-right: 1rem;
-    border-radius: 3rem;
-  }
+	box-sizing: border-box;
+	margin: 4rem;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	min-height: 100vh;
+	background-color: ${(props) => props.theme.secondaryColor};
+	color: ${(props) => props.theme.textColor};
+	padding: 2rem;
+	border-radius: 4rem;
+	@media screen and (max-width: 768px) {
+		margin-left: 1rem;
+		margin-right: 1rem;
+		border-radius: 3rem;
+	}
 `;
